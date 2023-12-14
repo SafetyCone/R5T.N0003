587c613c8ea5def28a5dd8a53f413e94443f95ba
davidcoats <david@getsafetycone.com>
2023-12-14 01:34:21 -0800

    Interim changes.

--- source/R5T.N0003/Code/Documentation.cs
+++ source/R5T.N0003/Code/Documentation.cs
@@ -8,5 +8,9 @@ namespace R5T.N0003
 	/// </summary>
 	public static class Documentation
 	{
+		/// <summary>
+		/// The R5T.N0003 namespace is reserved for location-anonymous strongly-typed types.
+		/// </summary>
+		public static readonly object ForLocationAnonymousStrongTypes;
 	}
 }
